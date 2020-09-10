6e42c26e3324d788321dd815e065191d8a7d1774
Christian Jones <cpj5199@psu.edu>
2020-09-10 16:59:12 +0000

    Auto Grader is not taking my code, so I tried something else

--- main.py
+++ main.py
@@ -5,37 +5,26 @@
 # Breakout: 14
 
 def getLetterGrade(grade):
-  if grade >= 93:
-    letter = "A"
-    print("Your letter grade for CMPSC 131 is " + letter + ".")
-  elif  grade >= 90:
-    letter = "A-"
-    print("Your letter grade for CMPSC 131 is " + letter + ".")
-  elif grade >= 87:
-    letter = "B+"
-    print("Your letter grade for CMPSC 131 is " + letter + ".")
-  elif grade >= 83:
-    letter = "B"
-    print("Your letter grade for CMPSC 131 is " + letter + ".")
-  elif grade >= 80:
-    letter = "B-"
-    print("Your letter grade for CMPSC 131 is " + letter + ".")
-  elif grade >= 77:
-    letter = "C+"
-    print("Your letter grade for CMPSC 131 is " + letter + ".")
-  elif grade >= 73:
-    letter = "C"
-    print("Your letter grade for CMPSC 131 is " + letter + ".")
-  elif grade >= 70:
-    letter = "C-"
-    print("Your letter grade for CMPSC 131 is " + letter + ".")
-  elif grade >= 60:
-    letter = "D"
-    print("Your letter grade for CMPSC 131 is " + letter + ".")
-  elif grade < 60:
-    letter = "F"
-    print("Your letter grade for CMPSC 131 is " + letter + ".")
-
+  if grade >= 93.0:
+    print("Your letter grade for CMPSC 131 is A.")
+  elif  grade >= 90.0:
+    print("Your letter grade for CMPSC 131 is A-.")
+  elif grade >= 87.0:
+    print("Your letter grade for CMPSC 131 is B+.")
+  elif grade >= 83.0:
+    print("Your letter grade for CMPSC 131 is B.")
+  elif grade >= 80.0:
+    print("Your letter grade for CMPSC 131 is B-.")
+  elif grade >= 77.0:
+    print("Your letter grade for CMPSC 131 is C+.")
+  elif grade >= 73.0:
+    print("Your letter grade for CMPSC 131 is C.")
+  elif grade >= 70.0:
+    print("Your letter grade for CMPSC 131 is C-.")
+  elif grade >= 60.0:
+    print("Your letter grade for CMPSC 131 is D.")
+  elif grade < 60.0:
+    print("Your letter grade for CMPSC 131 is F.")
 
 def run():
   grade = float(input("Enter your CMPSC 131 grade: "))
