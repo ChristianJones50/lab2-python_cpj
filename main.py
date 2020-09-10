846163f3c1a291964fc1fdd674f0bdb591056b54
Christian Jones <cpj5199@psu.edu>
2020-09-10 17:04:42 +0000

    Reverted back after fail

--- main.py
+++ main.py
@@ -6,25 +6,25 @@
 
 def getLetterGrade(grade):
   if grade >= 93.0:
-    return "Your letter grade for CMPSC 131 is A."
+    print("Your letter grade for CMPSC 131 is A.")
   elif  grade >= 90.0:
-    return "Your letter grade for CMPSC 131 is A-."
+    print("Your letter grade for CMPSC 131 is A-.")
   elif grade >= 87.0:
-    return "Your letter grade for CMPSC 131 is B+."
+    print("Your letter grade for CMPSC 131 is B+.")
   elif grade >= 83.0:
-    return "Your letter grade for CMPSC 131 is B."
+    print("Your letter grade for CMPSC 131 is B.")
   elif grade >= 80.0:
-    return "Your letter grade for CMPSC 131 is B-."
+    print("Your letter grade for CMPSC 131 is B-.")
   elif grade >= 77.0:
-    return "Your letter grade for CMPSC 131 is C+."
+    print("Your letter grade for CMPSC 131 is C+.")
   elif grade >= 73.0:
-    return "Your letter grade for CMPSC 131 is C."
+    print("Your letter grade for CMPSC 131 is C.")
   elif grade >= 70.0:
-    return "Your letter grade for CMPSC 131 is C-."
+    print("Your letter grade for CMPSC 131 is C-.")
   elif grade >= 60.0:
-    return "Your letter grade for CMPSC 131 is D."
+    print("Your letter grade for CMPSC 131 is D.")
   elif grade < 60.0:
-    return "Your letter grade for CMPSC 131 is F."
+    print("Your letter grade for CMPSC 131 is F.")
 
 def run():
   grade = float(input("Enter your CMPSC 131 grade: "))
